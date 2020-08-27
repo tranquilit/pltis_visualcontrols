dde796397f5c28fd3d0ed69bf761839c0342d96d
lbertaud <lbertaud@ad.tranquil.it>
2020-08-27 17:40:49 +0200

    Adding Useful properties on image positioning and cleaning repo

--- waptsearchedit/waptsearchedit.pas
+++ waptsearchedit/waptsearchedit.pas
@@ -15,15 +15,21 @@ type
   private
      FEmbeddedImage: TImage;
      FImagePanel: TPanel;
-     FSearchIconSize: Integer;
-     procedure SetSearchIconSize(Value: Integer);
+     FSearchIconSize: TConstraintSize;
+     FSearchIconSpacingLeft: TConstraintSize;
+     procedure SetUpEdit;
+     procedure SetUpPanel;
+     procedure SetUpImage;
+     procedure SetDefault;
+     procedure SetSearchIconSize(Value: TConstraintSize);
+     procedure SetSearchIconSpacingLeft(Value: TConstraintSize);
+     function GetSearchIconVisible: Boolean;
+     procedure SetSearchIconVisible(Value: Boolean);
+
 
   protected
       procedure SetParent(NewParent: TWinControl); override;
       procedure DoSetBounds(ALeft, ATop, AWidth, AHeight: integer); override;
-      procedure SetUpEdit;
-      procedure SetUpPanel;
-      procedure SetUpImage;
 
   public
      constructor Create(AOwner: TComponent); override;
@@ -31,16 +37,13 @@ type
   published
       property EmbeddedImage: TImage read FEmbeddedImage write FEmbeddedImage;
       property ImagePanel: TPanel read FImagePanel write FImagePanel;
-      property SearchIconSize: Integer read FSearchIconSize write SetSearchIconSize default 15;
+      property SearchIconSize: TConstraintSize read FSearchIconSize write SetSearchIconSize;
+      property SearchIconSpacingLeft: TConstraintSize read FSearchIconSpacingLeft write SetSearchIconSpacingLeft;
+      property SearchIconVisible: Boolean read GetSearchIconVisible write SetSearchIconVisible;
 
   end;
 
 procedure Register;
-
-var
-  default_x : Integer = 134;
-  default_y : Integer = 24;
-  FSearchIconSize: Integer = 15;
 
 implementation
 
@@ -51,11 +54,28 @@ begin
   RegisterComponents('Standard',[TWaptSearchEdit]);
 end;
 
-procedure TWaptSearchEdit.SetSearchIconSize(Value: Integer);
+procedure TWaptSearchEdit.SetSearchIconSize(Value: TConstraintSize);
 begin
   if FSearchIconSize = Value then exit;
   FSearchIconSize := Value;
   DoSetBounds(Left, Top, Width, Height);
+end;
+
+procedure TWaptSearchEdit.SetSearchIconSpacingLeft(Value: TConstraintSize);
+begin
+  if FSearchIconSpacingLeft = Value then exit;
+  FSearchIconSpacingLeft := Value;
+  DoSetBounds(Left, Top, Width, Height);
+end;
+
+function TWaptSearchEdit.GetSearchIconVisible: Boolean;
+begin
+  Result := FImagePanel.Visible;
+end;
+
+procedure TWaptSearchEdit.SetSearchIconVisible(Value: Boolean);
+begin
+  FImagePanel.Visible := Value;
 end;
 
 procedure TWaptSearchEdit.SetParent(NewParent: TWinControl);
@@ -68,22 +88,26 @@ end;
 procedure TWaptSearchEdit.DoSetBounds(ALeft, ATop, AWidth, AHeight: integer);
 begin
   inherited DoSetBounds(ALeft, ATop, AWidth, AHeight);
+
   if FEmbeddedImage = nil then exit;
   FEmbeddedImage.SetBounds(ALeft, ATop,  FSearchIconSize, FSearchIconSize);
 
   if FImagePanel = nil then exit;
-  FImagePanel.SetBounds(ALeft + AWidth - 5 - FSearchIconSize, ATop + 3, FImagePanel.Width, FImagePanel.Height);
+  FImagePanel.SetBounds(ALeft + AWidth - FSearchIconSpacingLeft - FSearchIconSize, ATop + (height div 2 - SearchIconSize div 2) - 1, FImagePanel.Width, FImagePanel.Height);
+
+  FImagePanel.Visible := (Width > (FSearchIconSize * 2 + 5));
 end;
 
 procedure TWaptSearchEdit.SetUpEdit;
 begin
-  self.SetBounds(self.Top, self.Left, default_x, default_y);
+  self.SetBounds(self.Top, self.Left, 130, 24);
 end;
 
 procedure TWaptSearchEdit.SetUpPanel;
 begin
   FImagePanel.ControlStyle := FImagePanel.ControlStyle + [csNoDesignSelectable];
   FImagePanel.AutoSize := true;
+  FImagePanel.Visible := true;
 end;
 
 procedure TWaptSearchEdit.SetUpImage;
@@ -92,12 +116,20 @@ begin
   FEmbeddedImage.Picture.Create;
   FEmbeddedImage.Picture.LoadFromResourceName(HINSTANCE,'SEARCH_ICON',TPortableNetworkGraphic);
   FEmbeddedImage.stretch := true;
+  FEmbeddedImage.Visible := true;
+end;
+
+procedure TWaptSearchEdit.SetDefault;
+begin
+  FSearchIconSize := 15;
+  width := 130;
+  FSearchIconSpacingLeft := 5;
 end;
 
 constructor TWaptSearchEdit.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
-  SetUpEdit;
+  SetDefault;
 
   if FImagePanel = nil then
   begin
@@ -105,9 +137,11 @@ begin
     SetUpPanel;
   end;
 
-  if FEmbeddedImage<>nil then exit;
-  FEmbeddedImage := TImage.Create(FImagePanel);
-  SetUpImage;
+  if FEmbeddedImage = nil then
+  begin
+    FEmbeddedImage := TImage.Create(FImagePanel);
+    SetUpImage;
+  end;
 end;
 
 
